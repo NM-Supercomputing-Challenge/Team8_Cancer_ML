be1f8f0f13637f26d9784c9f3073851ba70520dd
Tpool1 <tristenpool89@gmail.com>
2020-12-13 16:28:13 -0700

    first components of back-end integration

--- GUI.py
+++ GUI.py
@@ -14,13 +14,13 @@ title.place(relx=0.4,rely=0)
 
 font = tkFont.Font(family="Georgia",size=20)
 
-label = Label(text="Enter Data File",font=font)
+label = Label(text="Enter Data File path",font=font)
 label.place(relx=0.1,rely=0.1)
 
 entryBox = Entry()
 entryBox.place(relx=0.1,rely=0.15)
 
-label2 = Label(text="Enter another Data File",font=font)
+label2 = Label(text="Enter another Data File path",font=font)
 label2.place(relx=0.1, rely=0.2)
 
 entryBox2 = Entry()
@@ -31,5 +31,19 @@ label3.place(relx=0.1,rely=0.3)
 
 entryBox3 = Entry()
 entryBox3.place(relx=0.1,rely=0.35)
+
+class runImg():
+    def true(self):
+        run_img_model = True
+    def false(self):
+        run_img_model = False
+
+runImg = runImg()
+
+imgModelButton = Button(text="Image Model",command=runImg.true)
+imgModelButton.place(relx=0.1,rely=0.45)
+
+clinModelButton = Button(text="Clinical-Only Model",command=runImg.false)
+clinModelButton.place(relx=0.1,rely=0.5)
 
 window.mainloop()
