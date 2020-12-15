ab85a29ad8898284978fad60faf3ab9f70519ea5
Tpool1 <tristenpool89@gmail.com>
2020-12-15 12:18:53 -0700

    automated approach to GUI layout

--- GUI.py
+++ GUI.py
@@ -1,49 +1,73 @@
 from tkinter import *
 import tkinter.font as tkFont
+from tkinter import ttk
+
+with open("project_variables.txt","r") as projectVars:
+    vars=projectVars.readlines()
+
+print(vars)
 
 window = Tk()
 
-titleFont = tkFont.Font(family="Georgia",size=50)
+main_frame = Frame(window)
+main_frame.pack(fill=BOTH,expand=1)
 
-# Make title
-title = Label(text="Data Page",
-              font=titleFont,
-              fg="#11447a")
+canvas = Canvas(main_frame)
+canvas.pack(side=LEFT, fill=BOTH, expand=1)
 
-title.place(relx=0.4,rely=0)
+# Add a scrollbars to the canvas
+scrollbar = ttk.Scrollbar(main_frame, orient=VERTICAL, command=canvas.yview)
+scrollbar.pack(side=RIGHT, fill=Y)
 
-font = tkFont.Font(family="Georgia",size=20)
+scrollbar_x = ttk.Scrollbar(main_frame,orient=HORIZONTAL, command=canvas.xview)
+scrollbar_x.pack(side=BOTTOM, fill=X)
 
-label = Label(text="Enter Data File path",font=font)
-label.place(relx=0.1,rely=0.1)
+# Configure the canvas
+canvas.configure(xscrollcommand=scrollbar_x.set)
+canvas.configure(yscrollcommand=scrollbar.set)
+canvas.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
 
-entryBox = Entry()
-entryBox.place(relx=0.1,rely=0.15)
+second_frame = Frame(canvas)
+canvas.create_window((0,0), window=second_frame, anchor="nw")
 
-label2 = Label(text="Enter another Data File path",font=font)
-label2.place(relx=0.1, rely=0.2)
+def makeButton(name):
+    label = Label(second_frame,text=name)
+    label.pack()
 
-entryBox2 = Entry()
-entryBox2.place(relx=0.1,rely=0.25)
+    button = Button(second_frame,text="True")
+    button.pack()
 
-label3 = Label(text="Enter a string or list with target variable(s)",font=font)
-label3.place(relx=0.1,rely=0.3)
+    button = Button(second_frame,text="False")
+    button.pack()
 
-entryBox3 = Entry()
-entryBox3.place(relx=0.1,rely=0.35)
+def makeEntry(name):
+    label = Label(second_frame,text=name)
+    label.pack()
 
-class runImg():
-    def true(self):
-        run_img_model = True
-    def false(self):
-        run_img_model = False
+    entry = Entry(second_frame)
+    entry.pack()
 
-runImg = runImg()
+for var in vars:
+    var = str(var)
+    # checking if variable is a string
+    i = var.find('"')
 
-imgModelButton = Button(text="Image Model",command=runImg.true)
-imgModelButton.place(relx=0.1,rely=0.45)
+    # check if string contains tuple
+    t = var.find("(")
+    t2 = var.find(")")
 
-clinModelButton = Button(text="Clinical-Only Model",command=runImg.false)
-clinModelButton.place(relx=0.1,rely=0.5)
+    # check if string contains numbers
+    n = any(char.isdigit() for char in var)
+
+    # Only use part of variable before equal sign
+    var = var.rsplit('=', 1)[0]
+
+    if i == -1 and t == -1 and t2 == -1 and n == False:
+        makeButton(var[:-1])
+    else:
+        makeEntry(var[:-1])
+
 
 window.mainloop()
+
+
